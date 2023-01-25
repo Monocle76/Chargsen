a1afa3efbe02cf93a816e83dfb86861a3f7ce542
Monocle76 <monocle76@proton.me>
2023-01-25 18:40:08 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Heading, Text, Stack, Spacer } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW={"4xl"} mt={7}>
+      <Stack spacing={7} align={"start"}>
+        <Heading color={"teal.700"}>Welcome to my app!</Heading>
+        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolore, rem dolorem reiciendis perferendis maxime neque ducimus asperiores! Assumenda corrupti deserunt praesentium repellendus sint aperiam quas voluptatum qui voluptatibus?</Text>
+        <Button colorScheme={"teal"}>Load Data from server</Button>
+      </Stack>
+    </Container>
   );
 }
 
